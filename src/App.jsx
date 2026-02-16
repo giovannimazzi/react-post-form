c046a69b4778127b45dddb12574c24639d6d00e7
giovannimazzi <gioelor@libero.it>
2026-02-16 19:28:09 +0100

    gestiti alert formattati con bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { useState } from "react";
 const urlApi = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
 
 export default function App() {
+  const alertDefault = { content: "", type: "alert-info", isActive: false };
+  const [alertObj, setAlertObj] = useState(alertDefault);
+
   const initialState = {
     author: "",
     title: "",
@@ -19,6 +22,7 @@ export default function App() {
       [e.target.name]:
         e.target.type === "checkbox" ? e.target.checked : e.target.value,
     });
+    setAlertObj(alertDefault);
   };
 
   const handleFormSubmit = (e) => {
@@ -37,38 +41,80 @@ export default function App() {
     const printData = (objectData) => {
       let stringResult = "";
       for (let key in objectData) {
-        stringResult += `${key}: ${objectData[key]}\n`;
+        stringResult += ` ${key}: ${objectData[key]},`;
       }
-      return stringResult;
+      return stringResult.slice(0, -1);
     };
 
     const handleApiRequest = () => {
       {
+        /* alert("Dati Inviati!"); */
+        setAlertObj({
+          content: "Dati Inviati!",
+          type: "alert-info",
+          isActive: true,
+        });
         axios
           .post(urlApi, formData)
           .then((res) => {
             console.log(res.data);
-            alert("Risposta server: \n\n" + printData(res.data));
+            /* alert("Risposta server: \n\n" + printData(res.data)); */
+            setAlertObj((alertObj) => ({
+              ...alertObj,
+              content: "Risposta server:" + printData(res.data),
+              type: "alert-success",
+              isActive: true,
+            }));
           })
           .catch((e) => {
             console.error("ERRORE: " + e.message);
-            alert("ERRORE: \n\n" + e.message);
+            /* alert("ERRORE: \n\n" + e.message); */
+            setAlertObj((alertObj) => ({
+              ...alertObj,
+              content: "ERRORE: " + e.message,
+              type: "alert-danger",
+              isActive: true,
+            }));
           });
-        alert("Dati Inviati!");
       }
     };
 
-    missingData
-      ? alert(
+    if (missingData) {
+      /* alert(
           "Dati mancanti! Compilare in modo corretto tutti i campi e riprovare.",
-        )
-      : handleApiRequest();
+        ) */
+      setAlertObj({
+        content:
+          "Dati mancanti! Compilare in modo corretto tutti i campi e riprovare.",
+        type: "alert-warning",
+        isActive: true,
+      });
+    } else {
+      handleApiRequest();
+    }
 
     setFormData(initialState);
   };
 
   return (
     <div className="container p-5 vh-100">
+      <div className="alert-container">
+        <div
+          className={`alert alert-dismissible fade show ${alertObj.type} ${alertObj.isActive ? "" : "d-none"}`}
+          role="alert"
+        >
+          {alertObj.content}
+          <button
+            type="button"
+            className="btn-close"
+            aria-label="Close"
+            onClick={() =>
+              setAlertObj((alertObj) => ({ ...alertObj, isActive: false }))
+            }
+          ></button>
+        </div>
+      </div>
+
       <h1 className="text-center">📨Post Form📨</h1>
       <div className="card border-5">
         <form className="p-3" onSubmit={handleFormSubmit}>
